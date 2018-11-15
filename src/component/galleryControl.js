4419fdcf38acd889c4b4c22393a8527bc8f3abf4
Zyh <zhangyinghaoweb@163.com>
2018-11-15 17:23:53 +0800

    测试

--- src/component/galleryControl.js
+++ src/component/galleryControl.js
@@ -3,7 +3,7 @@ import '../assets/css/galleryControl.scss';
 
 class GalleryControl extends Component{
     render() {
-        return <div className="control">1</div>
+        return <div className="control"></div>
     }
 }
 export default GalleryControl
